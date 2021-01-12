770d61f3c33fdd6bbba3eff2db977c79dc785076
vasurajan <63305945+vasurajan@users.noreply.github.com>
2021-01-13 02:25:41 +0530

    scaffolding the app components

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import React from "react";
+import { Grid } from "semantic-ui-react";
+import "./App.css";
 
-function App() {
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+
+const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+      <SidePanel />
+
+      <Grid.Column style={{ marginLeft: 350 }}>
+      <Messages />
+      </Grid.Column>
+    </Grid>
   );
 }
 
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SidePanel extends Component {
+    render() {
+        return (
+            <div>
+                Side Panel
+            </div>
+        )
+    }
+}
+
+export default SidePanel
